bbfaea53b70a34e346f2c050f040e21ee53986ea
Maiko <maiko.mode05@gmail.com>
2023-09-12 18:39:28 +0400

    done

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int min = first > second ? second : first;
+        if (first == 0 || second == 0 ){
+            System.out.println(first == 0 ? second : first);
+        }else {
+            for (int i = min; i > 0; i--) {
+                if (first % i == 0 && second % i == 0) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -1,7 +1,20 @@
 package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
-    public void calculateSum(int lengthOfLastNumber) {
 
+    public  void calculateSum(int lengthOfLastNumber) {
+
+     int sum  = 0;
+     int pemt = 0;
+     for(int  i = 0; i< lengthOfLastNumber; i++ ){
+         pemt=pemt * 10 + 9;
+         sum+=pemt;
+     }
+
+     if(lengthOfLastNumber < 0){
+            System.out.println(0);
+     }else{
+         System.out.println(sum);
+     }
     }
 }
